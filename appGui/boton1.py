fc22f76869cd9df5100ea87403cfdfb0e967b715
Hugo Araya Carrasco <hugo.araya.carrasco@gmail.com>
2022-11-16 21:46:48 -0300

    16 Nov

--- appGui/boton1.py
+++ appGui/boton1.py
@@ -1,12 +1,11 @@
 from tkinter import *
-def funcion(n):
-    print(n)
 
 def funcion1():
     print('consola')
 
 if __name__== '__main__' :
     ventana = Tk()
+    ventana.geometry('200x200+100+100')
     boton = Button(ventana, text='Boton 1', command = funcion1)
     boton.grid(column=0, row=0)
     ventana.mainloop()
--- appGui/boton2.py
+++ appGui/boton2.py
@@ -2,12 +2,10 @@ import tkinter as tk
 def funcion(n):
     print(n)
 
-def funcion1():
-    print('consola')
-
 if __name__== '__main__' :
     ventana = tk.Tk()
-    x=5
+    ventana.geometry('200x200+100+100')
+    x = 5
     boton = tk.Button(ventana, text='Boton 1', command = lambda :funcion(x))
     boton.grid(column=0, row=0)
     ventana.mainloop()
